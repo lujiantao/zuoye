eb501cacdb3cc832ae279567abd5d8e3a92ec48d
lujiantao <616578707@qq.com>
2020-10-11 17:24:05 -0700

    week1

---
+++ 2-4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int main(){
+      int fd,i;
+      char buf[100];
+      fd=open("./test.data",O_CREAT|O_WRONLY,0644);
+      for(i=0;i<100;i++)
+	      buf[i]=i;
+      printf("process %d:before writing i=%d\n file description=%d\n",getpid(),i,fd);
+      i=write(fd,buf,sizeof(buf));
+      printf("after i=%d\n",i);
+      return 0;
+}
---
+++ text/1-2.s
@@ -0,0 +1,36 @@
+	.file	"1-2.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"hello"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L3:
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$99, -4(%rbp)
+	jle	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
